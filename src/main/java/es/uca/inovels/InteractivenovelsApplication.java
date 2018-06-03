f260fbd24899a7fac2648a35b7ba290de87c9c47
Francisco Fernández Sobejano <francisco.fernandezsobejano@alum.uca.es>
2018-06-03 14:15:46 +0200

    Primer commit

---
+++ src/main/java/es/uca/inovels/InteractivenovelsApplication.java
@@ -0,0 +1,12 @@
+package es.uca.inovels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractivenovelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractivenovelsApplication.class, args);
+	}
+}
